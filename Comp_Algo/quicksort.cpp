4b0550c2601831bcac7b0ae1293cf24ca64d1b60
Ashushingan <ashutoshshingan@gmail.com>
2025-07-28 11:30:45 +0530

    Comp_Algo[28/07]

--- Comp_Algo/quicksort.cpp
+++ Comp_Algo/quicksort.cpp
@@ -1,60 +1,29 @@
-#include <bits/stdc++.h>
+// quicksort.cpp
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-// Function to swap two elements
-void swap(int& a, int& b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-// Partition function
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // Choosing last element as pivot
-    int i = low - 1; // Index of smaller element
-
-    for (int j = low; j < high; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+void quicksort(vector<int>& arr, int left, int right) {
+    if (left >= right) return;
+    int pivot = arr[right];
+    int i = left - 1;
+    for (int j = left; j < right; ++j) {
+        if (arr[j] < pivot) {
+            swap(arr[++i], arr[j]);
         }
     }
-
-    swap(arr[i + 1], arr[high]);
-    return i + 1; // Pivot index
+    swap(arr[i + 1], arr[right]);
+    quicksort(arr, left, i);
+    quicksort(arr, i + 2, right);
 }
 
-// QuickSort function
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high); // Partitioning index
-        quickSort(arr, low, pi - 1);  // Recursively sort left part
-        quickSort(arr, pi + 1, high); // Recursively sort right part
-    }
-}
-
-// Function to print array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
-
-// Main function
 int main() {
-    int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
-
-    int arr[n];
-    cout << "Enter " << n << " numbers:\n";
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    quickSort(arr, 0, n - 1);
-
-    cout << "Sorted array: ";
-    printArray(arr, n);
-
+    srand(time(nullptr));
+    vector<int> arr(10000000);
+    for (int& x : arr) x = rand();
+    quicksort(arr, 0, arr.size() - 1);
     return 0;
 }
